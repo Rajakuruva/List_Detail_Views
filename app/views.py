c7cb95c241092c9f541d6c09ad1ed8f1451539d6
Rajakuruva <kuruvarajasekhar2019@gmail.com>
2023-05-19 19:07:47 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import *
+from app.models import *
+# Create your views here.
+class home(TemplateView):
+    template_name='app/Home.html'
+
+class School_list(ListView):
+    model=School
+    context_object_name='schools'
+
+class School_detail(DetailView):
+    model=School
+    context_object_name='sclobj'
